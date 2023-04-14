051182e390367366ab1a9bcee7da6dca03c2f225
Imprarce <tribendis.nikita@mail.ru>
2023-04-14 14:31:28 +0300

    Update#1

--- app/src/main/java/com/example/casemonitoring/Login_and_Password.java
+++ app/src/main/java/com/example/casemonitoring/Login_and_Password.java
@@ -1,20 +1,11 @@
 package com.example.casemonitoring;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Login_and_Password extends AppCompatActivity {
@@ -35,10 +26,10 @@ public class Login_and_Password extends AppCompatActivity {
         password = (EditText) findViewById(R.id.Password);
 
         enter.setOnClickListener(view -> {
-            if(login.getText().toString().equals("login") && password.getText().toString().equals("password")){
+           // if(login.getText().toString().equals("login") && password.getText().toString().equals("password")){
                 startActivity(new Intent(Login_and_Password.this, MainPage.class));
                 finish();
-            }
+          //  }
         });
 
     }
--- app/src/main/java/com/example/casemonitoring/MainPage.java
+++ app/src/main/java/com/example/casemonitoring/MainPage.java
@@ -1,11 +1,13 @@
 package com.example.casemonitoring;
 
-import static android.R.layout.simple_list_item_1;
 import static java.util.Arrays.asList;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,6 +28,10 @@ public class MainPage extends AppCompatActivity {
 
 
     private static final String TAG = "MainActivity";
+
+    private static List<String> caseInfoBase = new ArrayList<>();
+    private static final int MAX_PAGES = 4;
+    private static final String URL_FORMAT = "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Type%5B%5D=tag_CSGO_Type_WeaponCase&appid=730&q=case#p%d_price_asc";
     private static final List<String> URL = asList("https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Type%5B%5D=tag_CSGO_Type_WeaponCase&appid=730&q=case#p1_price_asc",
             "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Type%5B%5D=tag_CSGO_Type_WeaponCase&appid=730&q=case#p2_price_asc",
             "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Type%5B%5D=tag_CSGO_Type_WeaponCase&appid=730&q=case#p3_price_asc",
@@ -47,29 +53,32 @@ public class MainPage extends AppCompatActivity {
 
         refresh = (Button) findViewById(R.id.refreshCases);
 
-        refresh.setOnClickListener(view -> {
-            for(int i = 0; i < 4; i++) new SteamCasePriceParser().execute(URL.get(i));
-        });
+        String url = String.format(URL_FORMAT, 1);
+
+        refresh.setOnClickListener(view -> new SteamCasePriceParser().execute());
     }
 
-    private class SteamCasePriceParser extends AsyncTask<String, Void, List<String[]>> {
+    private class SteamCasePriceParser extends AsyncTask<Void, Void, List<String[]>> {
         final List<String[]> data = new ArrayList<>();
         @Override
-        protected List<String[]> doInBackground(String... urls) {
+        protected List<String[]> doInBackground(Void... voids) {
             try {
-                Document doc = (Document) Jsoup.connect(urls[0]).userAgent("Chrome/4.0.249.0 Safari/532.5").get();
-                Elements cases = doc.select(".market_listing_row_link");
-                for (Element caseItem : cases) {
-                    String caseName = caseItem.select(".market_listing_item_name").text();
-                    String casePrice = caseItem.select("span.market_table_value.normal_price > span.normal_price").text();
-                    String caseCount = caseItem.select(".market_listing_num_listings_qty").text();
-                    addData(caseName, casePrice, caseCount);
+                for (int page = 1; page <= MAX_PAGES; page++) {
+                    String url = String.format(URL_FORMAT, page);
+                    Document doc = (Document) Jsoup.connect(url).get();
+                    Elements cases = doc.select(".market_listing_row_link");
+                    for (Element caseItem : cases) {
+                        String caseName = caseItem.select(".market_listing_item_name").text();
+                        String casePrice = caseItem.select("span.market_table_value.normal_price > span.normal_price").text();
+                        String caseCount = caseItem.select(".market_listing_num_listings_qty").text();
+                        addData(caseName, casePrice, caseCount);
+                    }
                 }
-            } catch (IOException e) {
-                Log.e(TAG, "Error while parsing data from Steam website", e);
+                } catch(IOException e){
+                    Log.e(TAG, "Error while parsing data from Steam website", e);
+                }
+                return data;
             }
-            return data;
-        }
 
         private void addData(String name, String price, String count) {
             StringBuilder priceChange = new StringBuilder(price);
@@ -89,14 +98,27 @@ public class MainPage extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<String[]> data) {
-            List<String> outCase = new ArrayList<>();
             for (String[] row : data) {
-                String out = "Name - " + row[0] + ", Count - " + row[1] + ", Price - " + row[2];
-                outCase.add(out);
+                String out =  row[0] + " " + row[1] + " " + row[2];
+                caseInfoBase.add(out);
             }
-            final ArrayAdapter<String> adapter = new ArrayAdapter<>(MainPage.this,
-                    simple_list_item_1, outCase);
-            caseList.setAdapter(adapter);
+            if(caseInfoBase.size() > 30) {
+                final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainPage.this,
+                        android.R.layout.simple_list_item_1, caseInfoBase) {
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+
+                        TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                        textView.setTextColor(Color.WHITE);
+
+                        return view;
+                    }
+                };
+
+                caseList.setAdapter(adapter);
+            }
 
         }
     }
